7fbf8928c9ccbb01a13ce6b2d512ebf19f73917d
Manu <manudesignsforyou@gmail.com>
2025-10-12 00:19:17 +0200

    feat: Refactor service data and translations
    Updates the service data structure and corresponding translations to better represent the company's offerings. This includes renaming service IDs and text keys for clarity and consistency, and adjusting image URLs where necessary.

--- servicesData.ts
+++ servicesData.ts
@@ -9,45 +9,45 @@ export interface Service {
 
 export const services: Service[] = [
   { 
-    id: 'consulting',
-    titleKey: 'serviceConsulting',
-    descriptionKey: 'serviceConsultingDesc',
+    id: 'complete-renovations',
+    titleKey: 'serviceCompleteRenovations',
+    descriptionKey: 'serviceCompleteRenovationsDesc',
     image: 'https://firebasestorage.googleapis.com/v0/b/aedificia-nobile.firebasestorage.app/o/recursos%20web%2Fservicios%2FComplete%20Renovation%20Projects.jpg?alt=media' 
   },
   { 
-    id: 'bath-kitchen',
-    titleKey: 'serviceBathKitchen',
-    descriptionKey: 'serviceBathKitchenDesc',
+    id: 'kitchen-renovations',
+    titleKey: 'serviceKitchenRenovations',
+    descriptionKey: 'serviceKitchenRenovationsDesc',
     image: 'https://firebasestorage.googleapis.com/v0/b/aedificia-nobile.firebasestorage.app/o/recursos%20web%2Fservicios%2Fcocinas.jpg?alt=media'
   },
   { 
-    id: 'redistribution',
-    titleKey: 'serviceRedistribution',
-    descriptionKey: 'serviceRedistributionDesc',
+    id: 'space-redistribution',
+    titleKey: 'serviceSpaceRedistribution',
+    descriptionKey: 'serviceSpaceRedistributionDesc',
     image: 'https://firebasestorage.googleapis.com/v0/b/aedificia-nobile.firebasestorage.app/o/recursos%20web%2Fservicios%2FSpace%20Redistribution.jpg?alt=media'
   },
   { 
-    id: 'custom-design',
-    titleKey: 'serviceCustomDesign',
-    descriptionKey: 'serviceCustomDesignDesc',
+    id: 'custom-terrace-design',
+    titleKey: 'serviceCustomTerraceDesign',
+    descriptionKey: 'serviceCustomTerraceDesignDesc',
     image: 'https://firebasestorage.googleapis.com/v0/b/aedificia-nobile.firebasestorage.app/o/recursos%20web%2Fservicios%2Fterraza%20dise%C3%B1o.jpg?alt=media'
   },
   { 
-    id: 'painting',
-    titleKey: 'servicePainting',
-    descriptionKey: 'servicePaintingDesc',
+    id: 'painting-finishes',
+    titleKey: 'servicePaintingFinishes',
+    descriptionKey: 'servicePaintingFinishesDesc',
     image: 'https://firebasestorage.googleapis.com/v0/b/aedificia-nobile.firebasestorage.app/o/recursos%20web%2Fservicios%2FPainting%20and%20Finishes.jpg?alt=media'
   },
   { 
-    id: 'installations',
-    titleKey: 'serviceInstallations',
-    descriptionKey: 'serviceInstallationsDesc',
+    id: 'bathroom-renovations',
+    titleKey: 'serviceBathroomRenovations',
+    descriptionKey: 'serviceBathroomRenovationsDesc',
     image: 'https://firebasestorage.googleapis.com/v0/b/aedificia-nobile.firebasestorage.app/o/recursos%20web%2Fservicios%2Fba%C3%B1os.jpg?alt=media'
   },
   { 
-    id: 'windows',
-    titleKey: 'serviceWindows',
-    descriptionKey: 'serviceWindowsDesc',
+    id: 'roofs-pergolas',
+    titleKey: 'serviceRoofsPergolas',
+    descriptionKey: 'serviceRoofsPergolasDesc',
     image: 'https://firebasestorage.googleapis.com/v0/b/aedificia-nobile.firebasestorage.app/o/recursos%20web%2Fservicios%2Fpergolas.jpg?alt=media'
   },
 ];
